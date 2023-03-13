ab5d0f88b14334a21e263366d71ce353ab5ac631
Elain Hu <besthynlove@gmail.com>
2023-03-13 08:43:30 -0400

    m

--- console.js
+++ console.js
@@ -9,3 +9,34 @@ if (age >= 65) {
    //this block will run if no condition matches
    console.log('The value of the age variable is not numerical')
 }
+
+var day = `Sunday`;
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       //this block will run if no condition matches
+       console.log('There is no such day');
+}
+
+
+
