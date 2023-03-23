ab90f4308b4b98ed9a38a02606863d6d42d15bc4
Elain Hu <besthynlove@gmail.com>
2023-03-22 22:58:53 -0400

    m

--- practice1.js
+++ practice1.js
@@ -1,19 +1,9 @@
-function letterFinder(word, match) {
-    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
-    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
-    if(condition1 && condition2) { //if both condition matches
-        for(var i = 0; i < word.length; i++) {
-            if(word[i] == match) {
-                //check if the character at this i position in the word is equal to the match
-                console.log('Found the', match, 'at', i)
-            } else {
-                console.log('---No match found at', i)
-            }
-        }
-    } else {
-        //if the requirements don't match
-        console.log("Please pass correct arguments to the function")
+
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]) //display the array item where the index is euqal to i
     }
 }
-letterFinder([],[])
-letterFinder("cat","c")
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors); //display all items in the array at once
