4beb44d2f5c333f3ef72a874fd8f7e7f439ee6c4
Elain Hu <besthynlove@gmail.com>
2023-03-22 23:21:07 -0400

    m

--- practice1.js
+++ practice1.js
@@ -12,12 +12,11 @@ var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors); //display all items in the array at once
 
 function letterFinder(word, match) {
-    for(var i = 0; i < word.length; i++) {
-        if(word[i] == match) {
-            //check if the current characater, word[i], is equal to the match
-            console.log('Found the', match, 'at', i)
-        } else {
-            console.log('---No match found at', i)
+    for (var i = 0; i < word.length; i++){
+        if (word[i] == match){
+            console.log('found it', match, 'at', i)
+        }else{
+            console.log('no match at all', i)
         }
     }
 }
