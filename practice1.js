61d20462b14da1f9966e9f8bd15841ab57b5a384
Elain Hu <besthynlove@gmail.com>
2023-03-22 23:09:24 -0400

    m

--- practice1.js
+++ practice1.js
@@ -10,3 +10,16 @@ function listArrayItems(arr) {
 
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors); //display all items in the array at once
+
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //check if the current characater, word[i], is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+
+letterFinder("test", "t")
