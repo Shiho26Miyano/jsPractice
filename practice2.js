a0dd2218f1470b1624d69d8089cc940dc93fe1c1
Elain Hu <besthynlove@gmail.com>
2023-03-25 22:03:59 -0400

    m

--- practice2.js
+++ practice2.js
@@ -1,4 +1,10 @@
-var fruits = [];
-fruits.push("apple");
-fruits.push('pear');
-console.log(fruits);
+function arrayBuilder(one, two, three) {
+    var arr = [];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+    return arr;
+}
+
+var simpleArr = arrayBuilder('apple', 'pear', 'plum');
+console.log(simpleArr);
