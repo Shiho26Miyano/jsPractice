dee70710ad1f12fd9ea9e3a93b7fba76ffe2c16f
Elain Hu <besthynlove@gmail.com>
2023-03-26 18:46:00 -0400

    m

---
+++ 20230326_practice2.js
@@ -0,0 +1,25 @@
+// OOP1
+var purchase1 = {
+    shoes: 100,
+    stateTax:1.2,
+    totalPrice: function(){
+        var calculation = this.shoes * this.stateTax;
+        console.log('Total price:', calculation);
+    }
+}
+
+purchase1.totalPrice();
+
+
+// OOP2
+
+var purchase2 = {
+    shoes: 50,
+    stateTax: 1.2,
+    totalPrice: function(){
+        var calculation = this.shoes * this.stateTax;
+        console.log('Total price:', calculation);
+    }
+}
+purchase2.totalPrice();
+
