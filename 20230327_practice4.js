4e2d98a4ef7869c05fc50c9a51ba6fd2bef60308
Elain Hu <besthynlove@gmail.com>
2023-03-27 17:39:12 -0400

    m

--- 20230327_practice4.js
+++ 20230327_practice4.js
@@ -1,33 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@Shiho26Miyano 
-ginny100
-/
-Meta-Front-End-Developer
-Public
-Fork your own copy of ginny100/Meta-Front-End-Developer
-Code
-Issues
-Pull requests
-1
-Actions
-Projects
-Security
-Insights
-Meta-Front-End-Developer/Course 2 - Programming with JavaScript/Week 3 - Programming Paradigms/2.exercises/2.programming-assignment-building-an-object-oriented-program/ooprogramming.js /
-@ginny100
-ginny100 Reorder Week 3 of Course 2
-Latest commit 408203f on Oct 8, 2022
- History
- 1 contributor
-43 lines (37 sloc)  903 Bytes
-
 // Task 1: Code a Person class
 class Person {
     constructor(name = "Tom", age = 20, energy = 100) {
@@ -71,18 +41,3 @@ function manager() {
     manager.doSomethingFun();
     return manager;
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Meta-Front-End-Developer/ooprogramming.js at master · ginny100/Meta-Front-End-Developer
